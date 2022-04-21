ec159ab4c9669e25656ae8fc3453ddba91ed73a3
SidSherman <redneckchan@yandex.ru>
2022-04-21 17:17:09 +1200

    Fix input

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -14,13 +14,16 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] private Animator animatorController;
 
+    [SerializeField] private float jumpHeight;
     [SerializeField] private float movementSpeed; 
     [SerializeField] private Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         playerInput = new DefaultInput();
-        playerInput.Player.Movement.performed += context => OnMovement();
+        playerInput.Player.Enable();
+        playerInput.Player.Jump.started += Jump;
+        //playerInput.Player.Movement.performed += OnMovement;
     }
 
     public GameObject camera;
@@ -29,7 +32,7 @@ public class PlayerController : MonoBehaviour
     {
       //  float inputX = Input.GetAxis("Horizontal");
         //this.transform.Translate(new Vector3(5f * inputX, 0, 0) * Time.deltaTime);
-        camera.transform.position = new Vector3(this.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+       // camera.transform.position = new Vector3(this.transform.position.x, camera.transform.position.y, camera.transform.position.z);
        // Debug.Log(inputX);
         if (transform.position.y < -3f)
             transform.position = new Vector3(-5f,0.5f,0f);
@@ -39,19 +42,40 @@ public class PlayerController : MonoBehaviour
        // else 
         //    animatorController.SetBool("bIsMoving", false);
 
+        float inputMovement = playerInput.Player.Movement.ReadValue<float>();
+
+        
+        
+       // rigidbody.AddForce(new Vector3(inputMovement * movementSpeed,  0,0) * movementSpeed, ForceMode.Force);
+        Movement(inputMovement, movementSpeed);
 
     }
 
-    public void OnMovement ()
+        public void OnMovement (InputAction.CallbackContext context)
         {
 
         Debug.Log("OnMovement");
-        float inputMovement = playerInput.Player.Movement.ReadValue<float>();
-        rigidbody.transform.position = new Vector3(inputMovement * movementSpeed,  rigidbody.transform.position.y, rigidbody.transform.position.z) ;
+        Debug.Log(context);
+        
+        animatorController.SetFloat("MovementSpeed", Mathf.Abs(context.ReadValue<float>()));
+
+        }
+
+         public void Movement (float moveDirection, float velocity)
+        {
+
+            Vector2 dir = rigidbody.position + new Vector3(moveDirection, 0,0) * (Time.fixedDeltaTime * velocity);
+            rigidbody.transform.position = new Vector3(dir.x, rigidbody.transform.position.y, rigidbody.transform.position.z) ;
 
         }
     
+    public void Jump (InputAction.CallbackContext context)
+        {
 
+        Debug.Log("Jump");
+        Debug.Log(context);
+        rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
 
 
 
